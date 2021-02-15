105de49a35cb7643937492f7f34a306094bd473d
abdul <abdulghani.cse@gmail.com>
2021-02-15 11:18:42 +0300

    first java commit

---
+++ databasecon/JdbcTest.java
@@ -0,0 +1,39 @@
+package databasecon;
+
+import java.sql.*;
+
+
+public class JdbcTest {
+
+	
+	public static void main (String[] args)throws Exception {
+		
+String url="jdbc:mysql://localhost:3306/test";
+
+String user="root";
+String pass="root";
+String Query="select * from student";
+
+ Connection con=DriverManager.getConnection(url,user,pass);
+ 
+ Statement st=con.createStatement();
+ 
+ ResultSet rs=st.executeQuery(Query);
+ 
+ String stu_name;
+ int stu_id;
+ while(rs.next()){
+	 stu_name=rs.getString("name");
+	 stu_id=rs.getInt("sr_no");
+	 
+	 System.out.println(stu_id +" <- - -> " + stu_name);
+	 
+	 
+ }
+ 
+ rs.close();
+ st.close();
+ con.close();	
+	}
+	}
+
